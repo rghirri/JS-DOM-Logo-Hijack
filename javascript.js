d2b2bfd0816a77f13ca660ed5d6a19034ceff72a
Rayaa <rghirri@gmail.com>
2020-08-21 13:56:25 +0100

    First commit

---
+++ javascript.js
@@ -0,0 +1,5 @@
+var img = document.getElementById('logo-doodle-image');
+img.width = 400;
+img.src = 'http://www.logostage.com/logos/yahoo.GIF';
+var button = document.getElementById('logo-doodle-button');
+button.innerHTML = 'Yahoooo!';
